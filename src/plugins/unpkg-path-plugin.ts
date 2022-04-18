20f47757952b19fb0eb663c46e97371885b9fcf7
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-04-18 20:58:05 +0300

    Resolving Nested Paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,6 +6,7 @@ export const unpkgPathPlugin = () => {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       // определяет путь
+      // первый раз вызываем с path указанный в entryPoints: ["index.js"]
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResole", args);
         if (args.path === "index.js") {
@@ -15,7 +16,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(`${args.path}`, args.importer + "/").href,
+            path: new URL(
+              `${args.path}`,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
 
@@ -23,13 +27,6 @@ export const unpkgPathPlugin = () => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
       });
       // пытается загрузить контент файла по указанному пути
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -39,16 +36,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
