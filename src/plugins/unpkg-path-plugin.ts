8bcac1618dfb296bb5c5e57c8044b8ef58c6bb07
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-04-25 21:32:31 +0300

    Bundling User Input

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: "filecache",
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -40,11 +40,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return {
             loader: "jsx",
-            contents: `
-              import React from 'react';
-              const reactDOM = require('react-dom');
-              console.log(React);
-            `,
+            contents: inputCode,
           };
         }
 
